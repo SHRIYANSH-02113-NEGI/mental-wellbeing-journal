8fc345c16e5c135a4c4aec1aa5228f0291339c74
SHRIYANSH NEGI <162492217+SHRIYANSH-02113-NEGI@users.noreply.github.com>
2026-05-01 14:59:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+const journalRoutes = require("./routes/journalRoutes");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected ✅"))
-  .catch(err => console.log(err));
+// ✅ MongoDB Atlas connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Atlas connected ✅"))
+  .catch(err => console.error(err));
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
-});
+app.use("/", journalRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Backend running on http://localhost:${process.env.PORT}`);
 });
